33def811630aff97e87b22646349276aaf7529ad
germondai <bruno.krocek@gmail.com>
2024-05-08 21:52:56 +0200

    header highlight link based on section that takes the most of viewport

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -7,9 +7,10 @@
       class="xl:w-3/4 lg:w-[90%] w-[95%] h-14 px-8 flex items-center justify-between gap-8 rounded-[56px] bg-[#1f2023cc] backdrop-blur drop-shadow-md overflow-x-clip"
     >
       <ul class="w-full flex items-center justify-between gap-4">
-        <li v-for="(link, index) in $tm('header')" :key="index">
+        <li v-for="(link, index) in $tm('header')" :key="index" class="flex">
           <NuxtLink
             :href="$rt(link.href)"
+            :class="mvSect == $rt(link.name).toLowerCase() ? 'active' : ''"
             class="relative px-2 py-1 opacity-50 hover:opacity-100 transition-opacity"
           >
             {{ $rt(link.name) }}
@@ -56,6 +57,39 @@
   </header>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { height } = useWindowSize()
+const mvSect = ref('')
 
-<style lang="scss" scoped></style>
+onMounted(() => {
+  calculateLargestSection()
+  window.addEventListener('scroll', calculateLargestSection)
+})
+
+const calculateLargestSection = () => {
+  const sections = document.querySelectorAll('main > section')
+  const sectionsInfo: { y: number; id: string }[] = []
+
+  sections.forEach((section) => {
+    const { top, bottom } = useElementBounding(section as HTMLElement)
+    const elementPartHeight =
+      Math.min(bottom.value, height.value) - Math.max(top.value, 0)
+    sectionsInfo.push({
+      y: elementPartHeight,
+      id: section.id == 'timeline' ? 'about' : section.id,
+    })
+  })
+
+  mvSect.value = sectionsInfo.sort((a, b) => b.y - a.y)[0].id
+}
+</script>
+
+<style lang="scss" scoped>
+header nav a::after {
+  @apply content-[''] absolute left-0 bottom-1 w-full h-0.5 rounded-2xl bg-white opacity-0 duration-300 transition-opacity;
+}
+
+header nav a.active::after {
+  @apply opacity-70 hover:opacity-100;
+}
+</style>
