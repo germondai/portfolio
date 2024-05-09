77d923e8bf4ddf7b4b623b56b3fb3b35501262af
germondai <bruno.krocek@gmail.com>
2024-05-09 09:14:56 +0200

    btnScrollToTop -> tailwind

--- components/Btn/ScrollToTop.vue
+++ components/Btn/ScrollToTop.vue
@@ -13,36 +13,7 @@ const { y } = useWindowScroll({ behavior: 'smooth' })
 
 <style lang="scss">
 #btnScrollToTop {
-  background-image: url(/img/arrowTop-light.webp);
   background-size: calc(100% - 8px);
-  background-repeat: no-repeat;
-  background-position: center;
-  background-color: #1f2023cc;
-  display: flex;
-  position: fixed;
-  border-radius: 50%;
-  outline: none;
-  cursor: pointer;
-  right: 25px;
-  bottom: 25px;
-  width: 50px;
-  height: 50px;
-  padding: 4px;
-  font-size: 24px;
-  transition: background-color 0.5s;
-  z-index: 999;
-  border: solid #fff 4px;
-  box-shadow: 0 0 8px 4px rgba(0, 0, 0, 0.6);
-}
-
-#btnScrollToTop:hover {
-  background-color: #2b2244;
-}
-
-@media (max-width: 900px) {
-  #btnScrollToTop {
-    right: 10px;
-    bottom: 10px;
-  }
+  @apply fixed max-lg:bottom-3 bottom-6 max-lg:right-3 right-6 flex size-12 p-1 rounded-full text-2xl border-4 border-white z-[999] hover:bg-[#2b2244] bg-[#1f2023cc] bg-[url(/img/arrowTop-light.webp)] bg-no-repeat bg-center shadow-xl transition-colors duration-500;
 }
 </style>
