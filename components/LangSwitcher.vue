2a2489d5bca88f2f29ebc317cdcb291ec3b9c1e3
germondai <bruno.krocek@gmail.com>
2024-05-09 19:02:18 +0200

    swap locale fix + lil update

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-start gap-1">
-    <sub class="font-bold">{{ $t('welcome.select_language') }}</sub>
+    <sub class="font-bold">{{ $t('welcome.swap_language') }}</sub>
     <a
       v-for="langLocale in availableLocales"
       :key="langLocale.code"
