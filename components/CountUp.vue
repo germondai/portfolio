763813387313b3e6e12b7efe3afe50f019a1f41a
germondai <bruno.krocek@gmail.com>
2024-05-11 01:40:41 +0200

    new component CountUp

---
+++ components/CountUp.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="tag">
+    {{ count }}
+  </component>
+</template>
+
+<script lang="ts" setup>
+const { tag, number } = defineProps({
+  tag: {
+    type: String,
+    default: 'div',
+  },
+  number: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const count = ref(0)
+
+onMounted(() => {
+  const interval = setInterval(() => {
+    if (count.value < number) {
+      count.value++
+    } else {
+      clearInterval(interval)
+    }
+  }, 10)
+})
+</script>
+
+<style lang="scss"></style>
