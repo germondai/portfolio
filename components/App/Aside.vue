2a2bdce77dbecb75ae261a8f6cf46323634e6d72
germondai <bruno.krocek@gmail.com>
2024-05-20 10:34:13 +0200

    sidebar responsive fix

--- components/App/Aside.vue
+++ components/App/Aside.vue
@@ -2,32 +2,34 @@
   <DevOnly>
     <aside
       v-if="sections"
-      class="fixed lg:top-1/2 xs:top-[90%] max-xs:top-[94%] lg:left-[95%] max-lg:left-1/2 z-30"
+      class="flex max-md:w-[90%] fixed lg:top-1/2 xs:top-[90%] max-xs:top-[94%] lg:left-[95%] max-lg:left-1/2 z-30"
     >
       <FlareItem
-        class="p-0.5 rounded-2xl"
+        class="max-w-full mx-auto p-0.5 rounded-2xl"
         :after="{ color: '#ffffff11', size: 128 }"
         :before="{ color: '#ffffff44', size: 512 }"
       >
         <ul
           class="flex lg:flex-col items-center justify-center gap-2 p-2 bg-[#1d1c30cc] rounded-2xl"
         >
-          <li
-            v-for="(section, index) in sections"
-            :key="index"
-            :style="section.id == largestSection ? 'background: #45307f' : ''"
-          >
-            <NuxtLink :href="`#${section.id}`" :title="section.id">
-              <Icon
-                :name="
-                  $t(`sections.${section.id}`).includes(':')
-                    ? $t(`sections.${section.id}`)
-                    : 'line-md:question-circle'
-                "
-                class="size-8 opacity-80 hover:opacity-100"
-              />
-            </NuxtLink>
-          </li>
+          <ul class="flex lg:flex-col items-center gap-2 overflow-x-scroll">
+            <li
+              v-for="(section, index) in sections"
+              :key="index"
+              :style="section.id == largestSection ? 'background: #45307f' : ''"
+            >
+              <NuxtLink :href="`#${section.id}`" :title="section.id">
+                <Icon
+                  :name="
+                    $t(`sections.${section.id}`).includes(':')
+                      ? $t(`sections.${section.id}`)
+                      : 'line-md:question-circle'
+                  "
+                  class="size-8 opacity-80 hover:opacity-100"
+                />
+              </NuxtLink>
+            </li>
+          </ul>
           <template v-if="y > 500">
             <li class="divider"></li>
             <li>
@@ -51,7 +53,7 @@ aside {
 
   ul li {
     &:not(.divider) {
-      @apply grid place-items-center rounded-xl aspect-square size-12 overflow-hidden shadow-xl bg-[#2b2242] hover:bg-[#36275e] transition-colors;
+      @apply grid place-items-center rounded-xl aspect-square size-12 min-w-12 overflow-hidden shadow-xl bg-[#2b2242] hover:bg-[#36275e] transition-colors;
     }
 
     &.divider {
